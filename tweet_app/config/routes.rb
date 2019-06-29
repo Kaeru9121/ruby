0b8c4816d78e8d9747ca8f788001263e69cea912
大渕岬 <mi9s1a21@gamil.com>
2019-06-29 14:22:09 +0900

    新規投稿ページ作成

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  # トッップページ
   get "/" => "home#top"
+  # 紹介ページ
   get "about" => "home#about"
-  get 'index' => "posts#index"
+  # 投稿一覧ページ
+  get "index" => "posts#index"
+  # 新規投稿ページ
+  get "newPost" => "posts#newPost" 
+  # 投稿詳細ページ
+  get "show/:id" => "posts#show"
+  # 投稿保存用アクション
+  post "create" => "posts#create"
 end
