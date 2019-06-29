2b8596c5d4135ef0b3e7e752bc0ff46f9d8783d6
大渕岬 <mi9s1a21@gamil.com>
2019-06-29 16:14:44 +0900

    投稿編集/削除機能の追加

--- tweet_app/config/routes.rb
+++ tweet_app/config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   get "newPost" => "posts#newPost" 
   # 投稿詳細ページ
   get "show/:id" => "posts#show"
+  # 投稿編集ページ
+  get "edit/:id" => "posts#edit"
   # 投稿保存用アクション
   post "create" => "posts#create"
+  # 投稿更新用アクション
+  post "update/:id" => "posts#update"
+  # 投稿削除用アクション
+  post "destroy/:id" => "posts#destroy"
 end
